c3ff46b59fd35de41f115321f73798bd7ac8c57f
Ryan Brandenburg <rybrande@microsoft.com>
2017-04-18 17:04:34 -0700

    Add TryUpdateModel/TryValidateModel functional tests

---
+++ test/WebSites/RazorPagesWebSite/Pages/TryUpdateModelPageModel .cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesWebSite.Pages
+{
+    public class TryUpdateModelPageModel : PageModel
+    {
+        [ModelBinder]
+        public UserModel UserModel { get; set; }
+
+        public bool Updated { get; set; }
+
+        public async Task OnPost(UserModel user)
+        {
+            Updated = await TryUpdateModelAsync(user);
+        }
+    }
+}
